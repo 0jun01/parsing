ab893b683879ab57d8d207ba962cb741742609ba
0jun01 <dudwns85064843@gmail.com>
2024-08-19 17:46:32 +0900

    first commit

---
+++ src/main/java/com/tenco/bank/dto/Todo.java
@@ -0,0 +1,20 @@
+package com.tenco.bank.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@ToString
+public class Todo {
+	
+	private Integer userId;
+	private Integer id;
+	private String title;
+	private boolean completed;
+}
